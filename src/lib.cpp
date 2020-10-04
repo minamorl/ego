348a54520f979d7de6c2495bc1221bbf4817c52a
minamorl <minamorl@users.noreply.github.com>
2020-10-04 17:52:48 +0900

    Add an example

--- src/lib.cpp
+++ src/lib.cpp
@@ -1,5 +1,15 @@
+#include <boost/xpressive/xpressive.hpp>
+
 #include "lib.h"
 
 std::string greeting::greeting() {
-    return "Hello, world!";
+    using namespace boost::xpressive;
+
+    std::string s = "Hello, world!";
+    sregex rex = (s1= +_w) >> ", " >> (s2= +_w) >> "!";
+    smatch what;
+    if (regex_match(s, what, rex)) {
+        return what[1];
+    }
+    return "Fatal.";
 }
